d146361218c1706710a81f09ed5acf4c47b8fbc5
VeronikaOxyt <nikkimensh76@gmail.com>
2023-12-08 14:01:59 +0300

    changes from 02.12, Collections

--- src/game/Game2048.java
+++ src/game/Game2048.java
@@ -1,28 +1,65 @@
 package game;
 import board.Board;
 import direction.Direction;
-import java.util.Random;
+import key.Key;
+
+import java.util.*;
 
 public class Game2048 implements Game {
     private GameHelper helper = new GameHelper();
     private Board board;
-
     private Random random = new Random();
+
      public Game2048(Board board) {
          this.board = board;
      }
     @Override
-    public void init() {}
+    public void init() {
+        board.clear();
+        List<Integer> list = Arrays.asList(2, null, null, 2);
+        board.fillBoard(list);
+    }
     @Override
     public boolean canMove() {
-         return false;
+         return !board.availableSpace().isEmpty();
     }
     @Override
     public boolean move(Direction direction) {
-         return false;
+        if (canMove()) {
+            switch (direction) {
+                case LEFT :
+                    for (int i = 0; i < board.getWidth(); i++) {
+                        helper.moveAndMergeEqual(board.getValues(board.getRow(i)));
+                    }
+                    break;
+                case RIGHT :
+                    for (int i = board.getWidth()-1; i >= 0; i--) {
+                        helper.moveAndMergeEqual(board.getValues(board.getRow(i)));
+                    }
+                    break;
+                case UP :
+                    for (int j = 0; j < board.getHeight(); j++) {
+                        helper.moveAndMergeEqual(board.getValues(board.getColumn(j)));
+                    }
+                    break;
+                case DOWN :
+                    for (int j = board.getHeight() -  1; j >=0 ; j--) {
+                        helper.moveAndMergeEqual(board.getValues(board.getColumn(j)));
+                    }
+                    break;
+            }
+            addItem();
+        }
+        return canMove();
     }
     @Override
-    public void addItem() {}
+    public void addItem() {
+         List<Key> freeCells = board.availableSpace();
+        if (!freeCells.isEmpty()) {
+            int randomCell = random.nextInt(freeCells.size() - 1);
+            board.addItem(freeCells.get(randomCell), 2);
+        }
+    }
 
     @Override
     public Board getGameBoard() {
@@ -30,6 +67,6 @@ public class Game2048 implements Game {
     }
     @Override
     public boolean hasWin() {
-        return false;
+        return board.hasValue(2048);
     }
 }
